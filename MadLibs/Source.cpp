c5d1f1d3aba2e45b5481ada7b4b3354dd673b42d
Darian C <21974201+callahanDarian@users.noreply.github.com>
2018-03-27 13:32:25 -0500

    Initial commit

---
+++ MadLibs/Source.cpp
@@ -0,0 +1,30 @@
+/*
+MadLibs
+Date : 3.27.18
+Author : Darian Callahan
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string color;
+	std::string wordEst;
+	std::string bodyPartPlural;
+	std::string animal;
+	std::string noun;
+	std::string pluralNoun;
+	
+	int firstNumber;
+	int secondNumber;
+	int firstMinusSecond;
+	
+	std::cout << "The " << color << " dragon is the " << wordEst << " dragon of all. It has " << firstMinusSecond << std::endl;
+	std::cout << bodyPartPlural << " and a " << animal << " shaped like a " << noun << ", and it loves to eat " << std::endl;
+	std::cout << pluralNoun << ", although it will feast on nearly anything." << std::endl;
+	std::cout << std::endl;
+
+	system("pause");
+	return 0;
+}
